8e3df52cd44820eebaa98de09cc3b191bd69ddd8
Manikandan D <122381569+Manikandan-FE-Developer@users.noreply.github.com>
2024-02-22 16:54:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function downloadPDF() {
+    const pdfLink = 'https://drive.google.com/file/d/1J0ivtGXScKaIWIlCJshS48uE91w3E6Cd/export?format=pdf';
+
+    const link = document.createElement('a');
+    link.href = pdfLink;
+    link.download = 'Manikandan_CV.pdf';
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+}
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    document.getElementById('response').style.display = 'block';
+    document.querySelector('form').reset();
+
+    setTimeout(function() {
+        document.getElementById('response').style.display = 'none';
+    }, 5000);
+});
+
+const swiper = new Swiper('.swiper-container', {
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
