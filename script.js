e2e5b767971fc9478acc7b0fd42f2611d58ad69b
Manikandan D <122381569+Manikandan-FE-Developer@users.noreply.github.com>
2024-03-08 18:19:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-function downloadPDF() {
-    const pdfLink = 'https://drive.google.com/file/d/1J0ivtGXScKaIWIlCJshS48uE91w3E6Cd/export?format=pdf';
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    const resumeUrl = 'https://drive.google.com/uc?export=download&id=1ADutcr7UwsSQLbg59uCV7BTT4DNGGB5q';
 
     const link = document.createElement('a');
-    link.href = pdfLink;
+    link.href = resumeUrl;
     link.download = 'Manikandan_CV.pdf';
     document.body.appendChild(link);
 
     link.click();
 
     document.body.removeChild(link);
-}
-
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    document.getElementById('response').style.display = 'block';
-    document.querySelector('form').reset();
-
-    setTimeout(function() {
-        document.getElementById('response').style.display = 'none';
-    }, 5000);
 });
+
 
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
@@ -38,4 +28,17 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        document.getElementById('response').style.display = 'block';
+        document.querySelector('form').reset();
+
+        setTimeout(function() {
+            document.getElementById('response').style.display = 'none';
+        }, 5000);
+    });
 });
